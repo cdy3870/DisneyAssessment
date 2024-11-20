d11cdc63656756fef68bc10a413a731b0a3eea0d
cdy3870 <thefallen3870@yahoo.com>
2024-11-20 11:43:47 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/processed_offers.csv").drop(columns=["index", "Unnamed: 0"], axis=1)
-df.reset_index(inplace=True)
-
-print(df.columns)
-
-df.to_csv("data/processed_offers.csv", index=False)
